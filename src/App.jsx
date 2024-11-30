e97b7c965a5cc6f12472fd4e067fc3066a868e59
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2024-12-01 00:10:59 +0530

    create compornent

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
+import Hero from './components/hero/Hero'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
 
 function App() {
   
 
   return (
-    <>
-      <div>
-          <h1> hai my portfolio </h1>
+ 
+      <div className='' >
+          <Hero/>
+          <Services/>
+          <Portfolio />
+          <Contact/>
+
       </div>
      
-    </>
+
   )
 }
 
